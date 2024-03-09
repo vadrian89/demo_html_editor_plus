b76ee7a565e334f5f26ad1fef76197a78889e231
Adrian Verban <verbanady@gmail.com>
2024-03-09 15:27:26 +0200

    Updated theme

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,19 @@ import 'package:flutter/material.dart';
 
 import 'plus/example_scaffold.dart';
 
-void main() => runApp(
-      MaterialApp(
-        title: 'Html Editor Demo',
-        theme: ThemeData(useMaterial3: false),
-        darkTheme: ThemeData.dark(useMaterial3: false),
-        home: const HtmlEditorPlusExample(),
-      ),
-    );
+void main() {
+  const primaryColor = Color(0xFF009688);
+  final colorScheme = ColorScheme.fromSeed(seedColor: primaryColor);
+  final darkColorScheme = ColorScheme.fromSeed(
+    seedColor: primaryColor,
+    brightness: Brightness.dark,
+  );
+  runApp(
+    MaterialApp(
+      title: 'Html Editor Demo',
+      theme: ThemeData.from(colorScheme: colorScheme, useMaterial3: false),
+      darkTheme: ThemeData.from(colorScheme: darkColorScheme, useMaterial3: false),
+      home: const HtmlEditorPlusExample(),
+    ),
+  );
+}
